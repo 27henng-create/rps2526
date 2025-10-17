c1053bfb74682605cf5b4c1b7e0faa5cc34e7b3f
27henng-create <27henng@badinhs.org>
2025-10-17 13:49:18 -0400

    wrote instructions for project

--- rps.js
+++ rps.js
@@ -1,1 +1,17 @@
 // This is my javascript file for the rock, paper, scissors project
+
+
+// first things first
+// allow for the code to grab the players option using a function (prompt())
+// have the computer grab a random option out of "rock" "paper" or "scissors" (using Math.random())
+// write code about whenever you win or lose
+// e.x. "You win! :) Paper beats rock" or "You lose! :( Scissors beats Paper"
+// Paper > Rock
+// Rock > Scissors
+// Scissors > Paper
+
+
+// DOUBLE CHECK
+// if there are any eorrors in the code
+// if the code is stated multiple times in the console (use else if{})
+// if everything is SPELLED CORRECTLY 
